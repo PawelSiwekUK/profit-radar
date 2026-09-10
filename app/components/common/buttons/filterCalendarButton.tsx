e08bdd6b8d2b8711ed101c3f4558af5784980f60
PawDevUK <p.f.siwek@gmail.com>
2026-09-10 07:07:08 +0100

    refactor: remove Link wrapper from FilterButton for simplified structure

--- app/components/common/buttons/filterCalendarButton.tsx
+++ app/components/common/buttons/filterCalendarButton.tsx
@@ -5,12 +5,10 @@ import { ChevronDown } from 'lucide-react';
 export default function FilterButton({ item, onclick }: { item: { href: string; label: string }; onclick?: () => void }) {
 	return (
 		<div className='' onClick={onclick}>
-			<Link key={item.href} href={item.href} className={`flex `}>
-				<button className='calendar-filter-button'>
-					{item.label}
-					<ChevronDown size={20} />
-				</button>
-			</Link>
+			<button className='calendar-filter-button'>
+				{item.label}
+				<ChevronDown size={20} />
+			</button>
 		</div>
 	);
 }
